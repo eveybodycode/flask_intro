8c7d54c6c7f4700c39d7b69867cb22bbc945d9fd
humble327 <humblehenry1@gmail.com>
2022-08-30 14:06:06 +1400

    added route

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+from distutils.log import debug
 from flask import Flask
 
 app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello World"
+@app.route("/tuna/")
+def tuna():
+    return "live server"
 
-if __name__ == "__main__":
+if __name__=="__main__":
     app.run(debug=True)
